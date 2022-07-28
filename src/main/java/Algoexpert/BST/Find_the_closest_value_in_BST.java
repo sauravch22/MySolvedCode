a449304c3f73a5379a6eec06f59d46036a8135d5
sauravch22 <sauravchakraborty67@gmail.com>
2022-07-28 21:16:55 +0530

    My Solved Codes Created -> I will push every code I will solve

--- src/main/java/Algoexpert/BST/Find_the_closest_value_in_BST.java
+++ src/main/java/Algoexpert/BST/Find_the_closest_value_in_BST.java
@@ -1,4 +1,35 @@
-package Algoexpert.BST;
+import java.util.*;
 
-public class Find_the_closest_value_in_BST {
+class Program {
+    public static int findClosestValueInBst(BST tree, int target) {
+        int closestvalue = 0;
+        int absDiff = Integer.MAX_VALUE;
+        while(tree!=null){
+            int currentDiff = Math.abs(tree.value - target);
+            if(currentDiff<absDiff){
+                absDiff = currentDiff;
+                closestvalue = tree.value;
+            }
+            if(target == tree.value){
+                return tree.value;
+            }
+            else if(target > tree.value){
+                tree = tree.right;
+            }
+            else{
+                tree = tree.left;
+            }
+        }
+        return closestvalue;
+    }
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
 }
